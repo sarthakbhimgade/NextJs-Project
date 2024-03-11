52df93c34346654bc06ff76afcdc9ff018830366
Sarthak Bhimgade <sarthakbhimgade12@gmail.com>
2024-03-11 16:40:23 +0530

    update

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -5,8 +5,7 @@ export const authConfig = {
     signIn: '/login',
   },
   providers: [
-    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
-    // while this file is also used in non-Node.js environments
+    
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
